f939d9b2620beaa59b5d468ccd670b535d1ffa21
Polymathh <wambugum110@gmail.com>
2025-01-22 17:42:33 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    display.innerText.slice(0, -1) || '0';
+}
+
+function calculate() {
+    try{
+        display.innerText = eval(display.innerText.replace('x', '*').replace('÷', '/'));
+    } catch {
+        display.innerText = 'Error';
+    }
+}
